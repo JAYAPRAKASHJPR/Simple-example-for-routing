c46e0233da5e7983c8957972185785699871f1c5
JAYAPRAKASHJPR <jayaprakashssakthivel@gmail.com>
2021-07-11 13:35:44 +0530

    Update app.module.ts
    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent, HiComponent, TestComponent } from './hello.component';
+import {
+  ErrorComponent,
+  HelloComponent,
+  HiComponent,
+  TestComponent
+} from './hello.component';
 import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
-  { path: 'hello', component: HelloComponent },
+  {
+    path: 'hello',
+    component: HelloComponent,
+    children: [{ path: 'hellochild', component: HiComponent }]
+  },
   { path: 'hi', component: HiComponent },
-  { path: 'test', component: TestComponent }
+  { path: 'test', component: TestComponent },
+  { path: 'Page', component: ErrorComponent },
+  { path: '', component: TestComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: 'Page' }
 ];
 @NgModule({
   imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
